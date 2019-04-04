c9be3c8e45614e433be667efb83c51ecdadf81ca
WeiLee <wli_edu@163.com>
2019-04-04 13:54:37 +0800

    20190404 itelite

--- app/run.py
+++ app/run.py
@@ -2,3 +2,29 @@
 
 """
 __author__ = 'W. Li (liw@sicnu.edu.cn)'
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello world'
+
+
+@app.route('/')
+def index():
+    return 'Home Page'
+
+
+@app.route('/login')
+def login():
+    return 'Login Page'
+
+
+app.run()   # 运行Web服务器
+
+
--- prepare/app/run.py
+++ prepare/app/run.py
@@ -2,3 +2,34 @@
 
 """
 __author__ = 'W. Li (liw@sicnu.edu.cn)'
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/hello/')
+def hello():
+    return "Hello World"
+
+
+# app.add_url_rule('/hello', view_func=hello)
+
+
+@app.route('/')
+def index():
+    return "Home Page"
+
+
+@app.route('/login')
+def login():
+    return 'Login Page'
+
+
+app.run()
+
+
+
+
