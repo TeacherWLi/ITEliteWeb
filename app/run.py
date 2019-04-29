23164d76e360e6dba1c31be2349b10a90d15bb0a
LiWei <wli_edu@163.com>
2019-04-29 19:30:21 +0800

    login page

--- app/run.py
+++ app/run.py
@@ -17,13 +17,11 @@ def hello():
 @app.route('/')
 def index():
     return render_template('index.html')
-    #return '<html><body><h1>123<h1></body></html>'
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
-    #return 'Login Page'
 
 
 app.run(debug='True')   # 运行Web服务器
